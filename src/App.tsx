3ab9788fd90c2d71ffb7539d27c725680a0d968d
WallaceScott240 <wallacebusiness42@gmail.com>
2025-03-10 17:51:24 +0530

    n

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import About from './About'; // Create a placeholder About page
-import MyNavbar from './Components/RSA/Nav';
-import Home2 from './Components/RSA/PAGES/Home';
-import ContentSectionRsaLabs from './Components/RSA/About us/RsaLabs';
-import CareersPage from './Components/RSA/About us/CareerPage';
-import KeyServicesPage from './Components/RSA/About us/KeyServicesPage';
-import Research from './Components/RSA/About us/ResearchPage';
-import Patent from './Components/RSA/About us/PatentPage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MyNavbar from "./Components/RSA/Nav";
+import Home2 from "./Components/RSA/PAGES/Home";
+import ContentSectionRsaLabs from "./Components/RSA/About us/RsaLabs";
+import CareersPage from "./Components/RSA/About us/CareerPage";
+import KeyServicesPage from "./Components/RSA/About us/KeyServicesPage";
+import Research from "./Components/RSA/About us/ResearchPage";
+import Patent from "./Components/RSA/About us/PatentPage";
+import ScrollToTop from "./scrolltop"; // Import the ScrollToTop component
 
 const App: React.FC = () => {
   return (
     <Router>
+      <ScrollToTop /> {/* Add this component inside Router */}
       <MyNavbar />
       <Routes>
         <Route path="/" element={<Home2 />} />
@@ -20,9 +21,6 @@ const App: React.FC = () => {
         <Route path="/key-services" element={<KeyServicesPage />} />
         <Route path="/research-publication" element={<Research />} />
         <Route path="/ipr-patent" element={<Patent />} />
-        {/* <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<Contact />} /> */}
       </Routes>
     </Router>
   );
